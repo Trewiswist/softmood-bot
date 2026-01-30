d4e45eeb635259e16138ad3ad058b10db25bf8f3
Kristi Jons <110288445+Trewiswist@users.noreply.github.com>
2026-01-30 19:33:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = "8509609634:AAGnP3eDBGJREKRnU-2z6MvCkIGsy43-dI8"
+
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Я рядом 🤍")
